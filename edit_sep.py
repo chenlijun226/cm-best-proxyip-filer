30b6dff377b6ede6719b56e4eaf1e7dc8e21788b
andy <andy@example.com>
2026-06-04 11:18:11 +0800

    chore: exclude edit_*.py from tracking

--- edit_sep.py
+++
@@ -1,5 +0,0 @@
-﻿path = r"D:\sre-python\CheckProxyip\cm反代ip高优筛选订阅.py"
-content = open(path, encoding="utf8").read()
-content = content.replace('remark = f"{country_cn}+{city}+{asn}"', 'remark = f"{country_cn}-{city}-AS{asn}"')
-open(path, "w", encoding="utf8").write(content)
-print("Done")
